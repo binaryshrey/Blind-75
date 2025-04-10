68597961288338d414b244316f6c08ce25fa1f52
Shreyansh Saurabh <shreyansh.saurabh01@gmail.com>
2025-04-10 12:02:27 +0530

    001

---
+++ 001_contains_duplicate.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) != len(nums)
